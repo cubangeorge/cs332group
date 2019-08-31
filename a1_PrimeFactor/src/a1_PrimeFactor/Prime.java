6b570a43b2e8d7ac1fea10c838741818f23a5559
Jorge <jmartinq@masonlive.gmu.edu>
2019-08-31 19:40:40 -0400

    renamed get_if_prime to check_if_prime

--- a1_PrimeFactor/src/a1_PrimeFactor/Prime.java
+++ a1_PrimeFactor/src/a1_PrimeFactor/Prime.java
@@ -1,9 +1,10 @@
 package a1_PrimeFactor;
 
+
 public class Prime {
 	static int firstprime = 2; // used globally in this class
 
-	public static int get_if_prime(int num, int i) {
+	public static int check_if_prime(int num, int i) {
 		
 		if (num < firstprime)
 			return 0; 		// stop condition: return a zero since is less than smallest prime
@@ -16,10 +17,11 @@ public class Prime {
 		
 
 		
-		return get_if_prime(num, i + 1); /* Continues recursion */
+		return check_if_prime(num, i + 1); /* Continues recursion */
 	}
 
 	public static int is_prime(int num) {
-		return get_if_prime(num, firstprime); /* Initializes recursion */
+		
+		return check_if_prime(num, firstprime); /* Initializes recursion */
 	}
 }
--- a1_PrimeFactor/src/a1_PrimeFactor/PrimeTest.java
+++ a1_PrimeFactor/src/a1_PrimeFactor/PrimeTest.java
@@ -1,5 +1,6 @@
 package a1_PrimeFactor;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
