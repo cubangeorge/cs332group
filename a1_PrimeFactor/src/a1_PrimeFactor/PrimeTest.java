8f3d65fe6342d532ab4998745ef737ba15f9a4be
Jorge <jmartinq@masonlive.gmu.edu>
2019-08-31 19:10:36 -0400

    started assigment 1 this includes a function that tells if numbers are prime along with its tests

---
+++ a1_PrimeFactor/src/a1_PrimeFactor/PrimeTest.java
@@ -0,0 +1,29 @@
+package a1_PrimeFactor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PrimeTest {
+
+	@Test
+	void test_is_prime() {
+		
+		assertEquals(1,Prime.is_prime(2));//checking primes 
+		assertEquals(1,Prime.is_prime(3));
+		assertEquals(1,Prime.is_prime(5));
+		assertEquals(1,Prime.is_prime(7));
+		assertEquals(1,Prime.is_prime(11));
+		
+		assertEquals(0,Prime.is_prime(1));//check not primes less than 1st prime 
+		assertEquals(0,Prime.is_prime(0));
+	
+		assertEquals(0,Prime.is_prime(4));//check not primes more than first prime 
+		assertEquals(0,Prime.is_prime(6)); 
+		assertEquals(0,Prime.is_prime(10)); 
+
+		
+
+	}
+
+}
