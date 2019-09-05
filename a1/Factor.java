79fa008afc6e2ad39e3326cdb5a35e330fd40ba6
Valeria <raspberryfruitt@gmail.com>
2019-09-05 12:41:46 -0400

    Cleaned up the code to make it more readable and moved comments to above the methods.

--- a1/Factor.java
+++ a1/Factor.java
@@ -1,20 +1,35 @@
 
 public class Factor {
+	/*
+	 * this method HAS REVERSE LOGIC!!!!!
+	 * it will return 0 if b is a factor of a, or return 1 otherwise 
+	 */
 	public static int is_factor(int a, int b){
-		//this method HAS REVERSE LOGIC!!!!!
-		//it will return 0 if b is a factor of a, or return 1 otherwise 
 		
-		if (a<0) //flip a's sign if negative
-			a=a*-1;
-		if (b==0) {// this covers my zero edge cases since 0*0 = 0
-			
-			if (a==0) return 0;
-			else return 1;
+		//flip a's sign if negative
+		if (a < 0) { 
+			a = (a*-1);
+		}
+		
+		// this covers my zero edge cases since 0*0  = 0
+		if (b == 0) {
+			if (a == 0) { 
+				return 0; 
+			} else { 
+				return 1;
+			}	
 		} 
 		
-		if (b>a) return 1 ; // edge case where b is a greater number
-		if (b<0 && (a*a<b*b)) return 1; //case (4, -12) b is neg and abs(b)>abs(a)
-		return a%b;
+		// edge case where b is a greater number
+		if (b > a) { 
+			return 1; 
+		} 
+		//case (4, -12) b is neg and abs(b)>abs(a)
+		if ( b < 0 && ( (a * a) < (b * b) ) ) {
+			return 1; 
+		}
+		
+		return (a % b);
 				
 	}//end is_factor
 
--- a1/Prime.java
+++ a1/Prime.java
@@ -1,28 +1,30 @@
 
-
-
 public class Prime {
-	static int firstprime = 2; // used globally in this class
+	// used globally in this class
+	static int firstprime = 2;
 
 	public static int check_if_prime(int num, int i) {
 		
+		// stop condition: return a zero since is less than smallest prime
 		if (num < firstprime)
-			return 0; 		// stop condition: return a zero since is less than smallest prime
-
+			return 0; 		
+		
+		// stop condition: didn't find divisors other than self and one
 		else if (i > num - 1)
-			return 1; 		// stop condition: didn't find divisors other than self and one
+			return 1; 		
 		
+		// stop condition: return since found a divisor
 		else if (num % i == 0)
-			return 0; 		// stop condition: return since found a divisor
+			return 0; 		
 		
-
-		
-		return check_if_prime(num, i + 1); /* Continues recursion */
+		/* Continues recursion */
+		return check_if_prime(num, i + 1); 
 	}
 
+	/*this return 1 if number is prime zero otherwise*/
 	public static int is_prime(int num) {
-		/*this return 1 if number is prime zero otherwise*/
 		
-		return check_if_prime(num, firstprime); /* Initializes recursion */
+		 /* Initializes recursion */
+		return check_if_prime(num, firstprime);
 	}
 }
