366325e5cc18287a109d0c7994a0b12d494c1e76
Jorge <jmartinq@masonlive.gmu.edu>
2019-09-11 20:25:58 -0400

    removed unused imports leftover from unused typeCheckerTest

--- a2/Test_Suite.java
+++ a2/Test_Suite.java
@@ -7,16 +7,11 @@
 
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
-import java.util.Vector;
-
 import org.junit.jupiter.api.Test;
 
 
