58757c99bd1f2e3060edafd206737c7bedd409fa
Valeria <raspberryfruitt@gmail.com>
2019-09-12 13:38:34 -0400

    Altered my push method to work better as an immutable object by creating a new array with the old array's contents and an increased size to feed to a new object to be returned. Also, I added more test cases to ensure that the object references between the original and altered object are different and that thebehavior of the push method still stands.

--- a3/Stack.java
+++ a3/Stack.java
@@ -12,28 +12,38 @@ public class Stack {
 
    private Object[] elements;
 //   private final int size = 0; @Hi Vale: this size should change the original example had a final initial default value which is ok but not changing would be a problem
-   private final int size = 0;
+   private int size = 0;
 
 
    public Stack() {
      this.elements = new Object[0];
    }
    
-   public Stack(int size) {
-	     this.elements = new Object[size];
-	}
-   
-   public Stack(Object[] elements) {
+   public Stack(int size, Object[] elements) {
+	   	this.size = size;
 	     this.elements = elements;
 	}
    
-   
-   public Stack push (Object e) {
-	   Object[] newElements = this.elements.clone();
+   //takes an object of a type and produces a new one
+   //takes the old elements array and size of it to make a new array with the input parameter as the latest element. Then makes a new object based off of that.
+   public Stack push(Object e) {
+	   int newSize = elements.length + 1;
+	   Object newishElements[] = new Object[newSize];
+	   //System.arraycopy(elements, 0, newishElements, 0, newSize);
+	   for (int i = 0; i < elements.length; i++) {
+		   newishElements[i] = elements[i];
+	   }
 	   
-	   return new Stack(newElements);
+	   newishElements[newSize-1] = e;
+			   
+	   Stack newerStack = new Stack(newSize, newishElements);
+	     
+	    return newerStack;
    }
 
+   	public Object[] getElements() {
+   		return this.elements;
+   	}
 
 	public String toString() {
 	 String newString = "";
--- a3/Stack_Test_Suite.java
+++ a3/Stack_Test_Suite.java
@@ -16,32 +16,50 @@ import org.junit.jupiter.api.Test;
 
 
 public class Stack_Test_Suite {
-
 	
+	// ---------Altered immutable Stack class below -------
+
+	/*test for the push method in the Stack.java file
+	 * this is a test to see if the push method of awesome produces a new object
+	 */
 	@Test
 	void test_push_diff() {
 		Stack awesome = new Stack();
-		//Stack awesome2 = new Stack();
-		assertNotSame(awesome, awesome.push(2));
-		
-		
-		System.out.print(awesome.push(2).toString());
+		assertNotSame(awesome.push(2), awesome.push(2));
+	
 	}//end test
+	
+	
+	/*
+	 * test for the push method in the Stack.java file
+	*  tests to see if the push method is operating as fully intended
+	*/
+	@Test
+	void test_implementation_changes() {
+		Stack awesome = new Stack();
+
+		assertEquals(0, awesome.getElements().length);
+		assertEquals(1, awesome.push(2).getElements().length);
+		assertEquals(2, awesome.push(2).push(10).getElements().length);
+	}//end test
+	
+	
 	
 
+	// ---------Original mutable Stack class below -------
 	
-	/*@Test
-	void test_pop_diff() {
-		Stack awesome = new Stack();
-		assertNotSame(awesome, awesome.immutable_pop());
-		
-	}//end test
 	
+	//test for the push method in the Stack_Original.java file
 	@Test
-	void test_pop_same() {
-		Stack awesome = new Stack();
-		assertSame(awesome, awesome.mutable_pop());
+	void test_push_diff_stackOriginal() {
 		
-	}//end test*/
-	
+		Stack_Original awesome = new Stack_Original();
+		//method call in case elements is public
+		//assertEquals(0, awesome.elements.length);
+		awesome.push(2);
+		assertNotSame(awesome, awesome.pop());
+		//method call in case elements is public
+		//assertEquals(1, awesome.elements.length);
+	}//end test
+
 }
