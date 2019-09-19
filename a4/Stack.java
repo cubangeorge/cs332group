927f3d92443a5877c335f86ecee0af3f169e0ccf
Jorge <jmartinq@masonlive.gmu.edu>
2019-09-19 12:27:44 -0400

    edit

--- a4/Stack.java
+++ a4/Stack.java
@@ -41,8 +41,8 @@ public class Stack {
 		for (int i = 0; i < this.elements.length; i++) {
 			
 			newString += "|";
-			//newString += " Obj"+(i+1)+" ";//this.elements[i].toString();
-			newString += this.elements[i].toString();
+			newString += " Obj"+(i+1)+" ";//this.elements[i].toString();
+			//newString += this.elements[i].toString();
 			newString += "|";
 			//newString += ",";
 		}
