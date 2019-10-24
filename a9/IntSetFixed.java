1a8942a007b6712519bf43769e6ce99ab71b03f6
Jorge <jmartinq@masonlive.gmu.edu>
2019-10-24 12:58:18 -0400

    finished equals

--- a9/IntSetFixed.java
+++ a9/IntSetFixed.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class IntSetFixed implements Cloneable {  
@@ -27,9 +28,25 @@ public class IntSetFixed implements Cloneable {
 	   //check the size
 	   if (s.els.size() != this.els.size())
 		   	return false;
+	   //now proceed to check for the presence of each element in the target object
+	   Iterator<Integer> litr = s.els.listIterator(s.els.size());
+	
 	   
-	   
-	   return  ;
+	   while (litr.hasNext()) {
+		   int t = litr.next();
+		   boolean found = false;
+		   for (int i : this.els) {  
+			   if (t == i) {found = true; break; } 
+		   }//end for
+		   //if not found @ this point return false since its not present
+		   if (found ==false)   return false;	   
+		}//end while
+	   //from here down the entire list was search and 
+	   //the method should have returned false if any element was absent 
+	   //or if the sizes were different.
+	   //so at this point in execution both sizes are equal 
+	   //and each element is present which means the sets are equal
+	   return true ;
    }
    
 // @Override 
