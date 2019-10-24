8fd77a28e0f21e041def4541df35fa3548ca3620
Jorge <jmartinq@masonlive.gmu.edu>
2019-10-24 11:37:04 -0400

    added test stubs and class stubs

--- a9/IntSetFixed.java
+++ a9/IntSetFixed.java
@@ -1,35 +1,35 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class IntSet implements Cloneable {  
+public class IntSetFixed implements Cloneable {  
    private List<Integer> els;
-   public IntSet () { els = new ArrayList<Integer>(); }
+   public IntSetFixed () { els = new ArrayList<Integer>(); }
    //...
    @Override 
    public boolean equals(Object obj) {  // Standard recipe
-      if (!(obj instanceof IntSet)) return false;
+      if (!(obj instanceof IntSetFixed)) return false;
 
-      IntSet s = (IntSet) obj;
+      IntSetFixed s = (IntSetFixed) obj;
       return compare_size_and_elements(s);
    }
 
 
    // adding a private constructor
-   private IntSet (List<Integer> list) { els = list; }
+   private IntSetFixed (List<Integer> list) { els = list; }
 
    @Override 
-   public IntSet clone() { 
-      return new IntSet ( new ArrayList<Integer>(els));
+   public IntSetFixed clone() { 
+      return new IntSetFixed ( new ArrayList<Integer>(els));
    }
    
-   private boolean compare_size_and_elements(IntSet s) {
+   private boolean compare_size_and_elements(IntSetFixed s) {
 	   
 	   //check the size
 	   if (s.els.size() != this.els.size())
 		   	return false;
 	   
 	   
-	   return true ;
+	   return  ;
    }
    
 // @Override 
