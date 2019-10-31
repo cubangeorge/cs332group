5e607c0c15fd77b70327bc2b92e57f35eb400ef1
Jorge <jmartinq@masonlive.gmu.edu>
2019-10-31 01:42:07 -0400

    added class stubs

---
+++ aX/Point2.java
@@ -0,0 +1,16 @@
+public class Point2 {
+   private int x; // the x coordinate
+   private int y; // the y coordinate
+
+//   public boolean equals (Object p) { // overriding definition
+//      if (p instanceof Point3) return equals((Point3) p);
+//      return super.equals(p); }
+//
+//   public boolean equals (Point2 p) { // overriding definition
+//      if (p instanceof Point3) return equals((Point3) p);
+//      return super.equals(p); }
+//
+//   public boolean equals (Point3 p) { // extra definition
+//      if (p == null || z != p.z) return false;
+//      return super.equals(p); }
+}
--- aX/Point3.java
+++ aX/Point3.java
@@ -1,2 +1,15 @@
-public class Point3 extends Point2{
+public class Point3 extends Point2 {
+   private int z; // the z coordinate
+
+   public boolean equals (Object p) { // overriding definition
+      if (p instanceof Point3) return equals((Point3) p);
+      return super.equals(p); }
+
+   public boolean equals (Point2 p) { // overriding definition
+      if (p instanceof Point3) return equals((Point3) p);
+      return super.equals(p); }
+
+   public boolean equals (Point3 p) { // extra definition
+      if (p == null || z != p.z) return false;
+      return super.equals(p); }
 }
