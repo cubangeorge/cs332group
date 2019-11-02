d21542fcd8b14a4c48ab71804630b776457b8525
Blake Khan <blake@blakekhan.com>
2019-11-01 20:08:45 -0400

    Make compile and catch transitivity error

--- aX/Point3.java
+++ aX/Point3.java
@@ -1,22 +1,33 @@
 public class Point3 extends Point2 {
-  
 
-private int z; // the z coordinate
-  
-	public Point3(int x, int y) {
-		super(x, y);
-		z=0;
-	}
-   @Override 
-   public boolean equals (Object p) { // overriding definition
-      if (p instanceof Point3) return equals((Point3) p);
-      return super.equals(p); }
-  // @Override
-   public boolean equals (Point2 p) { // overriding definition
-      if (p instanceof Point3) return equals((Point3) p);
-      return super.equals(p); }
+    private int z; // the z coordinate
 
-   public boolean equals (Point3 p) { // extra definition
-      if (p == null || z != p.z) return false;
-      return super.equals(p); }
+    public Point3(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public boolean equals(Object p) { // overriding definition
+        if (p instanceof Point3) {
+            return equals((Point3) p);
+        }
+
+        return super.equals(p);
+    }
+
+    public boolean equals(Point2 p) { // overriding definition
+        if (p instanceof Point3) {
+            return equals((Point3) p);
+        }
+
+        return super.equals(p);
+    }
+
+    public boolean equals(Point3 p) { // extra definition
+        if (p == null || z != p.z) {
+            return false;
+        }
+
+        return super.equals(p);
+    }
 }
