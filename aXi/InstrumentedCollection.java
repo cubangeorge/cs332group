0ed776b4a8868d8b5f301ff3de111edda9c6f9e9
Jorge <jmartinq@masonlive.gmu.edu>
2019-11-07 13:44:37 -0500

    tests equals for the List

--- aXi/InstrumentedCollection.java
+++ aXi/InstrumentedCollection.java
@@ -5,7 +5,7 @@ import java.util.Set;
 public class InstrumentedCollection<E> extends ForwardingCollection<E>{
    private int addCount = 0;	
 
-   public InstrumentedCollection(Set<E> s){ super(s); }
+   public InstrumentedCollection(Collection<E> s){ super(s); }
    @Override public boolean add(E e){ addCount++; return super.add(e); }
    @Override public boolean addAll(Collection<? extends E> c) { 
          addCount += c.size(); return super.addAll(c); 
--- aXi/Tests.java
+++ aXi/Tests.java
@@ -1,20 +1,24 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
-/**
- * 
- */
 
-/**
- * @author jorge
- *
- */
 class Tests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented"); // TODO
+	void test_InstrumentedList_equals() {
+	
+		List <String> l = Arrays.asList(new String[] {"1","2"});
+		InstrumentedList<String> il = new InstrumentedList <String>(l);
+		assertEquals(l, il);
+		assertEquals(il, l);
+		l.equals(il);
+		System.out.println(il);
+		System.out.println(l);
 	}
 
 }
