263e917443204e795b6ea226eb6983087790e8e9
Jorge <jmartinq@masonlive.gmu.edu>
2019-11-10 14:24:57 -0500

    finished tests

--- aXi/Tests.java
+++ aXi/Tests.java
@@ -1,9 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Collection;
+import java.util.HashSet;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,54 +14,76 @@ import org.junit.jupiter.api.Test;
 
 class Tests {
 	@Test
+	/**
+	 * this answers question number 1 and 
+	 * shows that the equals method does work for list 
+	 */
 	void test_InstrumentedList_equals() {
 	
-		List <String> l = Arrays.asList(new String[] {"1","2"});
-		InstrumentedList<String> il = new InstrumentedList <String>(l);
-		assertEquals(l, il);
-		assertEquals(il, l);
-		l.equals(il);
-		System.out.println(il);
-		System.out.println(l);
+		ArrayList<String> list_1 = new ArrayList<String>();
+		list_1.add("1");
+		list_1.add("2");		
+		ArrayList<String> list_2 = new ArrayList<String>();
+		list_2.add("1");
+		list_2.add("2");		
+		InstrumentedList<String> il = new InstrumentedList <String>(list_1);
+		assertEquals(list_1, il); //compared to it self
+		assertEquals(il, list_1);// compared to it self 
+		assertEquals(list_2,il);// compared to another identical list
+		il.add("3");//changed the list 
+		assertEquals(list_1,il); // the change can be seen through the wrapper class
 	}
+	
+	/**
+	 * This answers question 2 
+	 * Shows that the equals method fails
+	 * for the intrumentedCollection class
+	 */
 	@Test
-	void test_addInstrumentedCollection() {
+	void test_collections() {
+		//testing instrumented collection against sets 
+		HashSet <Integer> s = new HashSet<Integer>();
+		s.add(1);
+		s.add(2);
+		InstrumentedCollection<Integer> ic = new InstrumentedCollection<Integer>(s);
 
-		Collection<Integer> set1 = new ArrayList<Integer>();
-		set1.add(3);
-		set1.add(9);
-		set1.add(38);
+		/*
+		 * The test below passes since it dispatches to the 
+		 * HashSetclass equals() inside the instrumented collection,
+		 *  which then compares it to 
+		 * an identical set using the set interface overridden 
+		 * equals. 
+		 */		
+		assertEquals(ic,(s));
+		/* the test below fails equality since it is dispatched to
+		 * the HashSet class which implements Set and 
+		 * thus checks if ic is an instance of Set, since it is 
+		 * not, then the test fails even though the class wraps
+		 * an identical HashSet. 
+		 */		
+		assertNotEquals(s,ic);
+		///testing it now again Lists 
+		ArrayList<Integer> l = new ArrayList<Integer>();
+		l.add(1);
+		l.add(2);
+		ic = new InstrumentedCollection<Integer>(l);	
 		
-		InstrumentedCollection<Integer> collection1 = new InstrumentedCollection<Integer>(new ArrayList<Integer>());
-		collection1.addAll(set1);
+		/*
+		 * The test below passes since it dispatches to the 
+		 * ArrayList class equals() inside the instrumented collection,
+		 *  which then compares it to 
+		 * an identical list using the List interface overRidden 
+		 * equals. 
+		 */
+		assertEquals(ic,l);//<--------here the test passes
 		
-		assertEquals(3, collection1.getAddCount());
+		/* the test below fails equality since it is dispatched to
+		 * the ArrayList class which implements List and 
+		 * thus checks if ic is an instance of List, since it is 
+		 * not, then the test fails even though the class wraps
+		 * an ArrayList which does. 
+		 */
+		assertNotEquals(l,ic);//<------this shows broken symmetry
 		
-		collection1.add(8);
-		collection1.add(39);
-		collection1.add(300);
-		assertEquals(6, collection1.getAddCount());
-		
-	}
-
-	@Test
-	void test_equalsInstrumentedCollection() {
-		Collection<Integer> set1 = new ArrayList<Integer>();
-		set1.add(3);
-		set1.add(9);
-		set1.add(38);
-		
-		InstrumentedCollection<Integer> collection1 = new InstrumentedCollection<Integer>(new ArrayList<Integer>());
-		collection1.addAll(set1);
-		
-		
-		ForwardingCollection<Integer> collection2 = new ForwardingCollection<Integer>(new ArrayList<Integer>());
-		collection2.addAll(set1);
-
-		assertEquals(3, collection1.getAddCount());
-		assertEquals(3, collection2.size());
-		
-		assertFalse(collection2.equals(collection1)); //not equal even if add the same things
-		assertFalse(collection2.equals(collection2)); //not equal even if add the same things
-	}
+	}//end test
 }
