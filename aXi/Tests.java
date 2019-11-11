b2f3f38d6a45cab8023475a501087a24a8d67ae5
Jorge <jmartinq@masonlive.gmu.edu>
2019-11-10 22:58:37 -0500

    fixed the transitivity test

--- aXi/Tests.java
+++ aXi/Tests.java
@@ -59,26 +59,6 @@ class Tests {
 		assertEquals (il,list_2);
 		assertEquals (list_2,ily);
 		assertEquals (il,ily);
-//		assertEquals(list_1, il);
-//		assertEquals(il, list_1);
-//		
-//		assertEquals(list_3, ily);
-//		assertEquals(ily, list_3);
-//		
-//		
-//		//x.equals(y) where x is il and y is list_2
-//		assertEquals(list_2,il);
-//		assertEquals(il, list_2);
-//	
-//		//y.equals(z) where y is list_2 and z is ily
-//		assertEquals(list_2, ily);
-//		assertEquals(ily, list_2);
-//		
-//		
-//		//x.equals(z) where x is il and z is ily
-//		assertEquals(il, ily);
-//		assertEquals(ily, il);
-//			
 	}
 	
 	/**
@@ -109,6 +89,7 @@ class Tests {
 		 * an identical HashSet. 
 		 */		
 		assertNotEquals(s,ic);
+		
 		///testing it now again Lists 
 		ArrayList<Integer> l = new ArrayList<Integer>();
 		l.add(1);
