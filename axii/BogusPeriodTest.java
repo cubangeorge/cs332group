068b2abc74127d14d211ff8ebd6978cedf28db97
Jorge <jmartinq@masonlive.gmu.edu>
2019-11-14 22:06:41 -0500

    fixed added authors

--- axii/BogusPeriodTest.java
+++ axii/BogusPeriodTest.java
@@ -1,17 +1,17 @@
 import org.junit.jupiter.api.Test;
 
 
-
 /**
- * @author jorge
- *
+ * @author G 01066284 Valeria L Green (VG)
+ * @author G 00402127 Jorge L Martinez (JM) 
+ * @author G 01082586 Blake Khan (BK)
  */
 class BogusPeriodTest {
 
 	@Test
 	void test()  {
 		BogusPeriod bp = new BogusPeriod();
-	    Period p = (Period) bp.deserialize(bp.serializedForm);
+	    Period p = (Period) BogusPeriod.deserialize(BogusPeriod.serializedForm);
 	    System.out.println(p);
 	}
 	
