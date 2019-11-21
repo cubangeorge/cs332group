8631b83bab50d6f7b04b5c55c053014b885c22cc
Valeria <raspberryfruitt@gmail.com>
2019-11-21 12:05:51 -0500

    Added skeleton to Template.java

--- axiii/Template.java
+++ axiii/Template.java
@@ -7,4 +7,13 @@
 
 public class Template {
 
+    public Template(String templateText) {
+    }
+
+    public void set(String variable, String value) {
+    }
+
+    public String evaluate() {
+        return null;
+    }
 }
