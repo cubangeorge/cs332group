8895a968048f409fad8fc2c15012989202e69b66
Valeria <raspberryfruitt@gmail.com>
2019-11-21 12:30:20 -0500

    Added up to 2.9 in the textbook.

--- axiii/Template.java
+++ axiii/Template.java
@@ -6,14 +6,18 @@
 
 
 public class Template {
+	
+	private String variableValue;
+	
 	public Template(String templateText) {
     }
 
     public void set(String variable, String value) {
+    	this.variableValue = value;
     }
 
     public String evaluate() {
-        return "Hello, Reader";
+        return "Hello, " + variableValue;
     }
 }
 
--- axiii/TestTemplate.java
+++ axiii/TestTemplate.java
@@ -16,5 +16,13 @@ class TestTemplate {
         template.set("name", "Reader");
         assertEquals("Hello, Reader", template.evaluate());
     }
+	
+	@Test
+	public void differentValue() throws Exception {
+		Template template = new Template("Hello, ${name}");
+		template.set("name", "someone else");
+		assertEquals("Hello, someone else", template.evaluate());
+		
+	}
 
 }
