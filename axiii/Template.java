80ab1ceb1a9d6a6448f66dccfde62507f43bf6bd
Jorge <jmartinq@masonlive.gmu.edu>
2019-11-21 12:15:34 -0500

    refactor

--- axiii/Template.java
+++ axiii/Template.java
@@ -6,5 +6,13 @@
 
 
 public class Template {
+	public Template(String templateText) {
+    }
 
+    public void set(String variable, String value) {
+    }
+
+    public String evaluate() {
+        return null;
+    }
 }
--- axiii/TestTemplate.java
+++ axiii/TestTemplate.java
@@ -1,17 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 /**
- * @author G 01066284 Valeria L Green (VG)
- * @author G 00402127 Jorge L Martinez (JM) 
- * @author G 01082586 Blake Khan (BK)
+ * 
  */
 
+/**
+ * @author jorge
+ *
+ */
+class TestTemplate {
 
-import org.junit.Test;
-public class TestTemplate {
 	@Test
     public void oneVariable() throws Exception {
         Template template = new Template("Hello, ${name}");
         template.set("name", "Reader");
         assertEquals("Hello, Reader", template.evaluate());
     }
+
 }
